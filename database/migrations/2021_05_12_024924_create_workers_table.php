7d61baaa60592defdf6437701a1eb464cacdab40
Wisdom OKechukwu <wisdom.okechukwu1998@gmail.com>
2021-05-11 16:20:41 -1100

    user migration update and worker migration

--- database/migrations/2021_05_12_024924_create_workers_table.php
+++ database/migrations/2021_05_12_024924_create_workers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateWorkersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,18 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('workers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('company')->unique();
-            $table->string('company-slug')->unique();
-            $table->string('status');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('status');
             $table->string('image')->nullable();
-            $table->timestamp('email_verified_at');
-            $table->rememberToken();
+            $table->bigInteger('Biz_id')->unsigned();
             $table->timestamps();
+            $table->foreign('Biz_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
@@ -35,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('workers');
     }
 }
