ee2e9679c2bd52449c390863c5cea7df613bd8c4
Wisdom OKechukwu <wisdom.okechukwu1998@gmail.com>
2021-05-11 17:38:00 -1100

    personal and team member task migrations

---
+++ database/migrations/2021_05_12_041402_create_team_member_tasks_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeamMemberTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('team_member_tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('task_detail');
+            $table->string('start_date');
+            $table->string('end_date');
+            $table->string('status');
+            $table->bigInteger('team_name_id')->unsigned();
+            $table->bigInteger('team_member_id')->unsigned();
+            $table->bigInteger('Biz_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('team_name_id')->references('id')->on('teams')->onDelete('cascade');
+            $table->foreign('team_member_id')->references('id')->on('team_members')->onDelete('cascade');
+            $table->foreign('Biz_id')->references('id')->on('users')->onDelete('cascade');
+        
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('team_member_tasks');
+    }
+}
---
+++ database/migrations/2021_05_12_043341_create_personals_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personals', function (Blueprint $table) {
+            $table->id();
+            $table->string('task_detail');
+            $table->bigInteger('worker_id')->unsigned();
+            $table->string('date');
+            $table->timestamps();
+            $table->foreign('worker_id')->references('id')->on('workers')->onDelete('cascade');
+           
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personals');
+    }
+}
