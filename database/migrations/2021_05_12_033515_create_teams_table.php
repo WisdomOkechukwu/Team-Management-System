991c6ff1d2ee4dfd69ccad8634a8a759df2780fd
Wisdom OKechukwu <wisdom.okechukwu1998@gmail.com>
2021-05-11 16:46:32 -1100

    teams and team member migrations

---
+++ database/migrations/2021_05_12_033515_create_teams_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->id();
+            $table->string('team_name');
+            $table->string('teamo_bjectives');
+            $table->string('status');
+            $table->string('start_date');
+            $table->string('end_date');
+            $table->bigInteger('Biz_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('Biz_id')->references('id')->on('users')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+}
---
+++ database/migrations/2021_05_12_034033_create_team_members_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeamMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('team_members', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('worker_id')->unsigned();
+            $table->bigInteger('team_id')->unsigned();
+            $table->string('status');
+            $table->bigInteger('Biz_id')->unsigned();
+
+            $table->foreign('worker_id')->references('id')->on('workers')->onDelete('cascade');
+            $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
+            $table->foreign('Biz_id')->references('id')->on('users')->onDelete('cascade');
+        
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('team_members');
+    }
+}
