377520b1082a7c7eef4a8d20ffd1fe9d9ba22243
Wisdom OKechukwu <wisdom.okechukwu1998@gmail.com>
2021-05-11 15:18:19 -1100

    migration update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,16 +16,14 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('company')->unique();
+            $table->string('company-slug')->unique();
             $table->string('status');
-            $table->string('image')->nullable();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email')->unique();
             $table->string('password');
-            $table->bigInteger('business_id')->unsigned();
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
-
-            $table->foreign('business_id')->references('id')->on('admins')->onDelete('cascade');
         });
     }
 
