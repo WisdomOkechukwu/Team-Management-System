9671dcc1422f19b42aa5bddbfded5a6193560585
Wisdom OKechukwu <wisdom.okechukwu1998@gmail.com>
2021-05-11 15:08:01 -1100

    migration for users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,10 +17,15 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('status');
+            $table->string('image')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->bigInteger('business_id')->unsigned();
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('business_id')->references('id')->on('admins')->onDelete('cascade');
         });
     }
 
